01ab410bc81b603b5f86fd31c43c53b7d612b9a8
Sunny Mui <sunnycmui@gmail.com>
2019-01-31 19:25:00 -0800

    setup skeleton MVVM knockout framework and fix a css issue with 100% height

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 // run the main app on window loaded
 
-// start the framework
+function initMap() {
+  let map;
+  // starting coordinates for san jose,ca
+  let start_loc = {
+    lat: 37.3361,
+    lng: -121.89100000000002
+  }
 
-// instantiate the google map
+  // instantiate the google map
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: start_loc,
+    zoom: 13,
+    mapTypeControl: false
+  });
+}
 
 // import the starting data
 
@@ -36,19 +49,55 @@ api_parameters.near +
 '&query='+
 api_parameters.query;
 
-console.log(request);
 fetch(request)
     .then(function(response) {
       return response.json();
     })
     .then(function(the_json){
       let string_result = JSON.stringify(the_json);
-      console.log(string_result);
+      // console.log(string_result);
       let bounds = the_json.response.suggestedBounds;
       let results = the_json.response.groups;
-      console.log(results);
+      // console.log(results);
     })
     .catch(function() {
       window.alert('api no worky');
         // Code for handling errors
     });
+
+// Knockout Web App
+
+function Taqueria(data){
+  this.name = ko.observable(data.name);
+  this.location = ko.observable(data.location);
+  this.foursquare_id = ko.observable(data.id);
+}
+
+function TaqueriaListViewModel() {
+  // DATA
+  let self = this;
+  const init_data_url = 'data.js';
+  self.taquerias = ko.observableArray([]);
+
+  // STATE
+
+  self.current_taqueria = ko.observable();
+
+  // OPERATIONS
+
+  self.get_init_data = function(data_url){
+    fetch(data_url)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(init_data){
+        let string_result = JSON.stringify(init_data);
+        console.log(init_data);
+      });
+  };
+
+  // Initialize View Model Defaults
+  self.get_init_data(init_data_url);
+}
+
+ko.applyBindings(new TaqueriaListViewModel());
