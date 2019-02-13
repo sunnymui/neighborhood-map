b839f7e073e2adc3d48fbb05647e8b2854ba6604
Sunny Mui <sunnycmui@gmail.com>
2019-02-13 15:32:20 -0800

    remove added in data.js

--- js/data.js
+++ js/data.js
@@ -50,56 +50,6 @@
         "lat": 37.304927,
         "lng": -121.921147
     }
-  },{
-    "id": {
-      "foursquare":"4c6cba011fb2a143dd71fce6",
-      "place": "ChIJu7mzHV4yjoARCfC68ow9Ge0"
-    },
-    "name": "Taquerias El Farolito",
-    "location": {
-      "lat": 37.294277324987966,
-      "lng": -121.83359321012993
-    }
-  },{
-    "id": {
-      "foursquare":"4b5b5916f964a52097f628e3",
-      "place": "ChIJ883MGVs0joARZ938ykfiIDw"
-    },
-    "name": "Tu Mero Mole",
-    "location": {
-      "lat": 37.260744996927784,
-      "lng": -121.93246259880927
-    }
-  },{
-    "id": {
-      "foursquare":"4d3112a4c75a6ea855e700ae",
-      "place": "ChIJD2my84UxjoARCcxWmahDn1E"
-    },
-    "name": "Chavez Supermarket",
-    "location": {
-      "lat": 37.24946385783139,
-      "lng": -121.84510853545174
-    }
-  },{
-    "id": {
-      "foursquare":"4a584ec8f964a52083b71fe3",
-      "place": "ChIJySHcKL3Mj4ARVSRHnldFlhU"
-    },
-    "name": "Angelou's Cafe & Grill",
-    "location": {
-      "lat": 37.3370628238638,
-      "lng": -121.88980192552506
-    }
-  },{
-    "id": {
-      "foursquare":"4b5c0c4cf964a520642229e3",
-      "place": "ChIJRUCNAYjMj4ARw0wntRmoX-0"
-    },
-    "name": "Taqueria Lorena's",
-    "location": {
-      "lat": 37.35838727754912,
-      "lng": -121.8901318184971
-    }
   }
 ]
 }
